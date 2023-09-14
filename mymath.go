230292088c2934a657872f1b7e12aec1d7af6078
fapcon <134641997+fapcon@users.noreply.github.com>
2023-09-14 17:49:54 +0300

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -1,33 +1,56 @@
-package mymath
+package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	x := 4.0
+	y := 2.0
+	Abs := Abs(x)
+	Max := Max(x, y)
+	Sqrt := Sqrt(x)
+	Yn := Yn(int(x), y)
+	fmt.Println(Abs)
+	fmt.Println(Max)
+	fmt.Println(Sqrt)
+	fmt.Println(Yn)
+}
 func Sqrt(x float64) float64 {
+	math.Sqrt(x)
 	return math.Sqrt(x)
 }
 
 func Ceil(x float64) float64 {
+	math.Ceil(x)
 	return math.Ceil(x)
 }
 
 func Floor(x float64) float64 {
+	math.Floor(x)
 	return math.Floor(x)
 }
 
 func Pow(x, y float64) float64 {
+	math.Pow(x, y)
 	return math.Pow(x, y)
 }
 
 func Max(x, y float64) float64 {
+	math.Max(x, y)
 	return math.Max(x, y)
 }
 
 func Min(x, y float64) float64 {
+	math.Min(x, y)
 	return math.Min(x, y)
 }
 func Abs(x float64) float64 {
+	math.Abs(x)
 	return math.Abs(x)
 }
 func Yn(x int, y float64) float64 {
+	math.Yn(x, y)
 	return math.Yn(x, y)
 }
